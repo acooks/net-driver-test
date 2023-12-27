42bfbe9d69f06c9d977bfbb5ba161bb671b41b44
Andrew Cooks <acooks@gmail.com>
2023-12-27 17:09:27 +1000

    rework mtu test
     * document trap where ip tx/rx is not checked
     * add test for unsupported MTUs

--- mtu.sh
+++ mtu.sh
@@ -1,26 +1,52 @@
 #!/usr/bin/bash
 
 # Iterate over a list of MTU sizes
+#
+# This test simply checks whether the driver reports that is has successfully
+# changed the MTU. It doesn't check whether the interface can send/receive data
+# after the change.
+#
+# This can trigger a driver bug where the device stops receiving data, but the
+# test will not detect it.
+#
+# FIXME: the supported MTUs are hardware defined.
 
 [[ $(type -t now) == function ]] ||
 	source $(dirname ${BASH_SOURCE[0]})/include.sh
 
 # Lets try a few MTU sizes
-declare -a MTUS=(604 1179 1180 1499 1500 1520 1650 8900 9000 9100 16384)
+declare -a SUPPORTED_MTUS=(604 1279 1280 1499 1500 1520 1650 8900 9000 9100 16383)
+#16384 is in which list?
+declare -a UNSUPPORTED_MTUS=(500 16385)
 
 ktap "KTAP version 1"
-ktap "1..${#MTUS[@]}"
-
-ktap "# Walking MTU sizes: ${MTUS[@]}"
+ktap "# walking mtu sizes: ${SUPPORTED_MTUS[@]}"
+ktap "1..${#SUPPORTED_MTUS[@]}"
 
 let N=1
-for MTU in ${MTUS[@]}; do
-	ktap "# $MTU"
-	ip link set dev $IFNAME mtu $MTU || fail "Failed to set MTU $MTU"
+for MTU in ${SUPPORTED_MTUS[@]}; do
+	ktap "# Checking supported $MTU"
+	ip link set dev $IFNAME mtu $MTU || fail "# Failed to set MTU $MTU"
 	(ip --oneline link show dev $IFNAME | grep "mtu $MTU") &&
 		ktap "ok $N MTU_$MTU" ||
 		ktap "not ok $N MTU_$MTU"
 	let N++
-	sleep 1
 done
+
+# Unsupported MTUs should return error
+ktap "KTAP version 1"
+ktap "# walking mtu sizes: ${UNSUPPORTED_MTUS[@]}"
+ktap "1..${#UNSUPPORTED_MTUS[@]}"
+
+let N=1
+for MTU in ${UNSUPPORTED_MTUS[@]}; do
+	ktap "# Checking unsupported $MTU"
+	ip link set dev $IFNAME mtu $MTU &&
+		ktap "not ok $N MTU_$MTU" ||
+		ktap "ok $N MTU_$MTU"
+	let N++
+done
+
+ip link set dev $IFNAME mtu 1500
+
 ktap "# MTU walk done"
